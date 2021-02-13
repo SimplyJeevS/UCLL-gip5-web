8e7afca3904d398db75e65c697e2254aaad1f319
snakehead007 <snakehead007@pm.me>
2021-02-13 18:53:38 +0100

    updated bericht model and resource

--- src/main/java/be/ucll/java/gip5/rest/BerichtResource.java
+++ src/main/java/be/ucll/java/gip5/rest/BerichtResource.java
@@ -1,17 +1,18 @@
 package be.ucll.java.gip5.rest;
 
 import be.ucll.java.gip5.dao.BerichtRepository;
+import be.ucll.java.gip5.dao.PersoonRepository;
 import be.ucll.java.gip5.dao.WedstrijdRepository;
 import be.ucll.java.gip5.exceptions.NotFoundException;
 import be.ucll.java.gip5.exceptions.ParameterInvalidException;
 import be.ucll.java.gip5.model.Bericht;
 import be.ucll.java.gip5.dto.BerichtDTO;
+import be.ucll.java.gip5.model.Persoon;
 import be.ucll.java.gip5.model.Wedstrijd;
 import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,11 +25,12 @@ public class BerichtResource {
     private Logger logger = LoggerFactory.getLogger(BerichtResource.class);
     private BerichtRepository berichtRepository;
     private WedstrijdRepository wedstrijdRepository;
-
+    private PersoonRepository persoonRepository;
     @Autowired
-    public BerichtResource(BerichtRepository berichtRepository, WedstrijdRepository wedstrijdRepository){
+    public BerichtResource(BerichtRepository berichtRepository, WedstrijdRepository wedstrijdRepository,PersoonRepository persoonRepository){
         this.berichtRepository = berichtRepository;
         this.wedstrijdRepository = wedstrijdRepository;
+        this.persoonRepository = persoonRepository;
     }
 
     @GetMapping(value = "/v1/bericht/{id}")
@@ -61,6 +63,10 @@ public class BerichtResource {
         if(!wedstrijd.isPresent()){
             throw new NotFoundException("Wedstrijd with id "+bericht.getWedstrijdId());
         }
+        Optional<Persoon> persoon = persoonRepository.findPersoonById(bericht.getAfzenderId());
+        if(!persoon.isPresent()){
+            throw new NotFoundException("Persoon with id"+bericht.getAfzenderId().toString());
+        }
         Bericht newBericht = berichtRepository.save(new Bericht.BerichtBuilder()
         .boodschap(bericht.getBoodschap())
         .wedstrijdId(bericht.getWedstrijdId())
@@ -74,27 +80,40 @@ public class BerichtResource {
             summary = "Pas bericht aan",
             description = "Verander de inhoud van een bericht"
     )
-    public ResponseEntity putBericht(@PathVariable("id") Long id,@RequestBody BerichtDTO berichtDto) throws ParameterInvalidException, NotFoundException {
+    public ResponseEntity putBericht(@PathVariable("id") Long id,@RequestBody String boodschap, Long wedstrijdId ) throws ParameterInvalidException, NotFoundException {
         logger.debug("PUT request voor bericht gekregen");
         if(id == null && !(id instanceof Long) && id <=0 ){
             throw new ParameterInvalidException(id.toString());
         }
-        if(berichtDto.getBoodschap().isEmpty() || berichtDto.getWedstrijdId() == null){
-            throw new ParameterInvalidException(berichtDto.toString());
-        }
-        Optional<Wedstrijd> wedstrijd = wedstrijdRepository.findWedstrijdById(berichtDto.getWedstrijdId());
-        if(!wedstrijd.isPresent()){
-            throw new NotFoundException("Wedstrijd with id "+berichtDto.getWedstrijdId());
-        }
-        Optional<Bericht> bericht = berichtRepository.findBerichtById(id);
 
+        if(boodschap.isEmpty()){
+            throw new ParameterInvalidException(boodschap);
+        }
+
+        if(wedstrijdId == null || wedstrijdId <= 0){
+            throw new ParameterInvalidException(wedstrijdId.toString());
+        }
+
+        Optional<Wedstrijd> wedstrijd = wedstrijdRepository.findWedstrijdById(wedstrijdId);
+        if(!wedstrijd.isPresent()){
+            throw new NotFoundException("Wedstrijd with id "+wedstrijdId);
+        }
+
+        Optional<Bericht> bericht = berichtRepository.findBerichtById(id);
         if(!bericht.isPresent()){
             throw new NotFoundException(id.toString());
         }
-        bericht.get().setBoodschap(berichtDto.getBoodschap());
-        bericht.get().setWedstrijdId(berichtDto.getWedstrijdId());
+
+        bericht.get().setBoodschap(boodschap);
+        bericht.get().setWedstrijdId(wedstrijdId);
+
         berichtRepository.save(bericht.get());
-        return ResponseEntity.status(HttpStatus.OK).body(new BerichtDTO(bericht.get().getWedstrijdId(),bericht.get().getBoodschap()));
+        return ResponseEntity.status(HttpStatus.OK).body(new BerichtDTO(
+                wedstrijdId,
+                boodschap,
+                bericht.get().getAfzenderId(),
+                bericht.get().getTijdstip()
+        ));
     }
 
     @DeleteMapping(value="/v1/bericht/{id}")
