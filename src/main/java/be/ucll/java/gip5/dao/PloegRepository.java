ee6998b70d4edfa898c4f01529bbc818642ad320
snakehead007 <snakehead007@pm.me>
2021-02-14 11:26:51 +0100

    add 4 ploeg requests

--- src/main/java/be/ucll/java/gip5/dao/PloegRepository.java
+++ src/main/java/be/ucll/java/gip5/dao/PloegRepository.java
@@ -4,9 +4,11 @@ import be.ucll.java.gip5.model.Ploeg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PloegRepository extends JpaRepository<Ploeg, Long> {
     Optional<Ploeg> findPloegById(Long id);
+    List<Ploeg> findAll();
 }
--- src/main/java/be/ucll/java/gip5/rest/PloegResource.java
+++ src/main/java/be/ucll/java/gip5/rest/PloegResource.java
@@ -1,6 +1,7 @@
 package be.ucll.java.gip5.rest;
 
 import be.ucll.java.gip5.dao.PloegRepository;
+import be.ucll.java.gip5.dto.PloegDTO;
 import be.ucll.java.gip5.exceptions.NotFoundException;
 import be.ucll.java.gip5.exceptions.ParameterInvalidException;
 import be.ucll.java.gip5.model.Ploeg;
@@ -10,11 +11,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -43,6 +42,60 @@ public class PloegResource {
             return ResponseEntity.status(HttpStatus.OK).body(ploeg.get());
         }else{
             throw new NotFoundException(id.toString());
+        }
+    }
+
+    @GetMapping( value = "/v1/ploeg")
+    public ResponseEntity getPloegen() throws NotFoundException {
+        List<Ploeg> ploegen = ploegRepository.findAll();
+        if(ploegen.isEmpty()){
+            throw new NotFoundException("Geen ploegen gevonden");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(ploegen);
+    }
+
+    @PostMapping(value="/v1/ploeg")
+    public ResponseEntity postPloeg(@RequestBody PloegDTO ploeg) throws ParameterInvalidException {
+        checkPloegDTO(ploeg);
+        Ploeg newPloeg = ploegRepository.save(
+                new Ploeg.PloegBuilder()
+                .naam(ploeg.getNaam())
+                .build()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(newPloeg);
+    }
+
+    @PutMapping( value = "/v1/ploeg/{id}")
+    public ResponseEntity putPloeg(@PathVariable("id") Long id,@RequestBody PloegDTO ploeg) throws ParameterInvalidException, NotFoundException {
+        if(id == null && !(id instanceof Long) && id <=0 ){
+            throw new ParameterInvalidException(id.toString());
+        }
+        Optional<Ploeg> foundPloeg = ploegRepository.findPloegById(id);
+        if(!foundPloeg.isPresent()){
+            throw new NotFoundException("Ploeg met id "+id);
+        }
+        checkPloegDTO(ploeg);
+        foundPloeg.get().setNaam(ploeg.getNaam());
+        ploegRepository.save(foundPloeg.get());
+        return ResponseEntity.status(HttpStatus.OK).body(foundPloeg);
+    }
+
+    @DeleteMapping( value = "/v1/ploeg/{id}")
+    public ResponseEntity deletePloeg(@PathVariable("id") Long id) throws NotFoundException, ParameterInvalidException {
+        if(id == null && !(id instanceof Long) && id <=0 ){
+            throw new ParameterInvalidException(id.toString());
+        }
+        Optional<Ploeg> foundPloeg = ploegRepository.findPloegById(id);
+        if(!foundPloeg.isPresent()){
+            throw new NotFoundException("Ploeg met id "+id);
+        }
+        ploegRepository.delete(foundPloeg.get());
+        return ResponseEntity.status(HttpStatus.OK).body(foundPloeg);
+    }
+
+    private void checkPloegDTO(PloegDTO ploeg) throws ParameterInvalidException {
+        if(ploeg.getNaam().isEmpty() || ploeg.getNaam().trim().length() ==0){
+            throw new ParameterInvalidException("Naam met value "+ploeg.getNaam());
         }
     }
 }
