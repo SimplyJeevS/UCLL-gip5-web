3374b6219e177996c3bc04af81a68902bb67400d
snakehead007 <snakehead007@pm.me>
2021-02-15 13:07:15 +0100

    fix properites

--- src/main/java/be/ucll/java/gip5/config/SpringFoxConfig.java
+++ src/main/java/be/ucll/java/gip5/config/SpringFoxConfig.java
@@ -1,7 +1,10 @@
 package be.ucll.java.gip5.config;
 
+import com.helger.commons.io.resource.ClassPathResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.Resource;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -18,4 +21,5 @@ public class SpringFoxConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+
 }
