afb7a67f34c7c874e3c2489c07bbe6ffaf6c9eaf
JensVannerum <r0807519@ucll.be>
2021-03-06 17:44:07 +0100

    Login + Home

--- src/main/java/be/ucll/java/gip5/controller/PersoonController.java
+++ src/main/java/be/ucll/java/gip5/controller/PersoonController.java
@@ -1,15 +1,11 @@
 package be.ucll.java.gip5.controller;
 
 import be.ucll.java.gip5.dto.PersoonDTO;
-import be.ucll.java.gip5.model.Rol;
 import com.vaadin.flow.spring.annotation.VaadinSessionScope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
-import java.sql.Date;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
 @VaadinSessionScope
@@ -20,12 +16,6 @@ public class PersoonController {
     private PersoonDTO loggedInUser;
 
     private static List<PersoonDTO> users;
-
-    static {
-        users = new ArrayList<>(1);
-        users.add(new PersoonDTO("admin", "admin", Date.from(Instant.now()), "M", "Bergenhof 56",
-                "0486947755", "0486947755", "admin@admin.com", "admin", Rol.SECRETARIS));
-    }
 
     public PersoonDTO authenticateUser(PersoonDTO unauthenticateduser) {
         for (PersoonDTO user : users) {
